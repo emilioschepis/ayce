5681a45040265ecd4acf2db5b8547db70014f132
Emilio Schepis <emilio.schepis@gmail.com>
2022-10-01 22:59:29 +0200

    feat: improve avatar image

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -9,12 +9,12 @@ type Props = {
 const Avatar: React.FC<Props> = ({ email, display_name, image_url }) => {
   if (image_url) {
     return (
-      <div className="relative h-8 w-8 overflow-hidden rounded-full">
+      <div className="relative h-12 w-12 overflow-hidden rounded-full border-2 border-gray-500">
         <Image
           src={image_url}
           alt={display_name ?? email}
-          width={32}
-          height={32}
+          width={48}
+          height={48}
           className="object-cover"
         />
       </div>
@@ -24,7 +24,7 @@ const Avatar: React.FC<Props> = ({ email, display_name, image_url }) => {
   const initials = (display_name ?? email).substring(0, 2);
 
   return (
-    <div className="relative flex h-8 w-8 items-center justify-center overflow-hidden rounded-full bg-gray-300">
+    <div className="relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-2 border-gray-500 bg-gray-300 text-sm font-bold uppercase">
       <p>{initials}</p>
     </div>
   );
